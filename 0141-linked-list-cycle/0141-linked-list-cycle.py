aa71bd994b3c45008ee1e748164ce6fb66ef4481
Sakthi <89055838+Sakthi51@users.noreply.github.com>
2023-10-20 12:37:00 +0530

    Time: 59 ms (48.43%), Space: 20.4 MB (74.78%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,15 +6,29 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        hashset = set()
-
-        curr = head
-        while curr:
-            hashset.add(curr)
-            curr = curr.next
-            if curr in hashset:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+            if slow == fast:
                 return True
         return False
+
+# Time complexity - O(n)
+# Space complexity - O(1)
+
+# # Hashset solution: 
+#         hashset = set()
+
+#         curr = head
+#         while curr:
+#             hashset.add(curr)
+#             curr = curr.next
+#             if curr in hashset:
+#                 return True
+#         return False
         
-# Time Complexity - O(n)
-# Space Complexity - O(n)
+# # Time Complexity - O(n)
+# # Space Complexity - O(n)
+        
